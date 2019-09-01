9464e460866b43d01aeda7c830b8ff530d451a84
Rocha Leandro <llrocha@desarrollosocial.gob.ar>
2019-09-01 13:35:24 -0300

    remove

--- resources/views/template/messages.blade.php
+++
@@ -1,9 +0,0 @@
-@if($errors->any())
-    <div class="alert bg-warning  alert-dismissible" id="messages">
-        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-
-        @foreach($errors->all() as $error)
-          <li>{{$error}}</li>
-        @endforeach
-    </div>
-@endif
